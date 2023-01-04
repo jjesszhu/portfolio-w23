fedf93bc1b2f23ac35d92e2386586c8cbbee5404
Jessica Zhu <jessica.jz.zhu@gmail.com>
2023-01-04 17:41:14 -0500

    add hamburger on mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { faHamburger } from "@fortawesome/free-solid-svg-icons";
 import "./navBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav-link-container">
-        <a href="#about" className="nav-link">about</a>
-        <a href="#work" className="nav-link">work</a>
-        <a className="nav-link">résumé</a>
+        <div
+          className="burger"
+          onClick={() => {
+            setIsOpen(!isOpen);
+            console.log(isOpen);
+          }}
+        >
+          <FontAwesomeIcon icon={faHamburger} size="2x" />
+        </div>
+        <a href="#about" className={`nav-link ${!isOpen ? "hidden" : ""}`}>
+          about
+        </a>
+        <a href="#work" className={`nav-link ${!isOpen ? "hidden" : ""}`}>
+          work
+        </a>
+        <a href="#work" className={`nav-link ${!isOpen ? "hidden" : ""}`}>résumé</a>
       </div>
     </nav>
   );
