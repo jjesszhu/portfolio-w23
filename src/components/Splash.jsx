6400081f060d2c316a16d7c2600f444ab20a0413
Jessica Zhu <jessica.jz.zhu@gmail.com>
2023-01-04 12:59:19 -0500

    create hero section

---
+++ src/components/Splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./splash.css";
+
+const Splash = () => {
+    return (
+        <header className="splash-background">
+            <h2 className='splash-small-text'>a website about</h2>
+            <h1 className="splash-big-text">JESSICA ZHU</h1>
+            <a className="splash-link" href="https://smittenkitchen.com/" rel="noreferrer" target="_blank">résumé</a>
+        </header>
+    );
+};
+
+export default Splash;
