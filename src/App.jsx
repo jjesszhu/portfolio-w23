7d2404df985f15177dcb9bd24d09d9dff36c232c
Jessica Zhu <jessica.jz.zhu@gmail.com>
2023-01-04 15:41:00 -0500

    conditionally render film diary, for now

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,31 @@ import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Splash from "./components/Splash";
 import WorkSummary from "./components/Work";
-import letterboxdClient from "./data/letterboxd";
+// import letterboxdClient from "./data/letterboxd";
 
 const App = () => {
   const [filmList, setFilmList] = useState([]);
 
-  useEffect(() => {
-    const response = letterboxdClient();
-    if (response) {
-      setFilmList(response);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const response = letterboxdClient();
+  //   if (response) {
+  //     setFilmList(response);
+  //   }
+  // }, []);
 
   return (
     <div className="App">
         <NavBar />
         <Splash />
         <About />
-        <FilmDiary
-          filmList={filmList}
-        />
-        <hr className="dashed-divider"/>
+        {!filmList.length === 0 && (
+          <>
+            <FilmDiary
+              filmList={filmList}
+            />
+            <hr className="dashed-divider"/>
+          </>
+        )}
         <WorkSummary />
         <Footer />
     </div>
