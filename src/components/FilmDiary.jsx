e1078d4c33a32d0a482c240f82eef25c7e483f41
Jessica Zhu <jessica.jz.zhu@gmail.com>
2023-01-04 12:58:59 -0500

    modify film diary styling

--- src/components/FilmDiary.jsx
+++ src/components/FilmDiary.jsx
@@ -1,49 +1,41 @@
-import React from 'react';
-import "./FilmDiary.css"
+import React from "react";
+import "./filmDiary.css";
 
-const FilmEntry = ({
-    title,
-    poster,
-    watchDate
-}) => {
-    return (
-        <div className="film-entry-container">
-            <img
-                className="film-poster"
-                src={poster}
-                alt={`${title} poster`}
-                loading="lazy"
+const FilmEntry = ({ title, poster, watchDate }) => {
+  return (
+    <div className="film-entry-container">
+      <img
+        className="film-poster"
+        src={poster}
+        alt={`${title} poster`}
+        loading="lazy"
+      />
+      <div className="film-detail-container">
+        <p className="film-title">{title}</p>
+        <p className="watch-date">{watchDate}</p>
+      </div>
+    </div>
+  );
+};
+
+const FilmDiary = ({ title = "film diary", filmList }) => {
+  return (
+    <section>
+      <h3 className="film-diary-title">{title}</h3>
+      <div className="film-diary">
+        {filmList?.map((entry, index) => {
+          return (
+            <FilmEntry
+              key={index}
+              title={entry.film.title.toUpperCase()}
+              poster={entry.film.image.large}
+              watchDate={new Date(entry.date.watched).toDateString()}
             />
-            <div className="film-detail-container">
-                <p className="film-title">{title}</p>
-                <p className='watch-date'>{watchDate}</p>
-            </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </section>
+  );
 };
-
-const FilmDiary = ({
-    title = "film diary",
-    filmList
-}) => {
-    return (
-        <>
-            <h3 className="film-diary-title">{title}</h3>
-            <div className="film-diary">
-                {filmList?.map((entry, index) => {
-                    const date = new Date(entry.date.watched).toDateString();
-                    return (<FilmEntry
-                        key={index}
-                        title={entry.film.title.toUpperCase()}
-                        poster={entry.film.image.small}
-                        watchDate={date}
-                    />);
-                })} 
-            </div>
-        </>
-    );
-};
-
-
 
 export default FilmDiary;
