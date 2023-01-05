6579d5046388a3ec8e88d8f3ab238092be903b87
Jessica Zhu <jessica.jz.zhu@gmail.com>
2023-01-05 13:25:36 -0500

    content updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ const About = () => {
           <div className="about-content-container">
             <img className="about-img" src={aboutImage} alt="me, not even close to catching a frisbee"/>
             <p className="about-content">
-              I'm a second year SYDE student @ <a className="about-link" href="https://uwaterloo.ca/systems-design-engineering/" rel="noreferrer" target="_blank">uWaterloo</a> on my way through 2A <strong>(and seeking spring 2023 jobs!)</strong>.
+              Hello! I'm Jessica (she/her), and I'm a second year SYDE student @ <a className="about-link" href="https://uwaterloo.ca/systems-design-engineering/" rel="noreferrer" target="_blank">uWaterloo</a> on my way through 2A <strong>(and seeking summer 2023 jobs!)</strong>.
               <br/><br/>
               I've previously worked at <a className="about-link" href="https://fordpro.com/en-us/" rel="noreferrer" target="_blank">Ford</a> as a SWE intern, and <a className="about-link" href="https://uwaterloo.ca/systems-design-engineering/" rel="noreferrer" target="_blank">OpenText</a> as a mobile developer intern.
               <br/><br/>
-              When I'm not (reluctantly) embracing the UW grind, I'm usually watching basketball, learning how to knit or going to the movies. 
+              When I'm not (reluctantly) embracing the UW grind, I'm watching basketball, learning how to knit, baking or going to the movies. 
             </p>
           </div>
           
